841b641c9a33d0b8b1c33e9ebac35cf7ee1d5e94
sina.ghorbanian <sinaleag79@gmail.com>
2024-01-23 10:55:00 +0330

    Added __init__ , apiCaller and manipulator methods

--- main.py
+++ main.py
@@ -3,4 +3,24 @@ import json
 
 
 class geoCoding:
-    pass
+    def __init__(self, city, country):
+        self.city = city
+        self.country = country
+        self.apiCaller()
+
+    def apiCaller(self):
+        self.url = "http://api.openweathermap.org/geo/1.0/direct"
+        self.apiKey = "1bcff021a86229cf441db3cfed0fdf88"
+        self.response = requests.get(
+            self.url, params={"q": self.city + "," + self.country, "appid": self.apiKey}
+        )
+        self.manipulator()
+
+    def manipulator(self):
+        self.jsonResponse = self.response.json()
+        self.lat = self.jsonResponse[0]["lat"]
+        self.lon = self.jsonResponse[0]["lon"]
+
+
+gc = geoCoding("mashhad", "iran")
+print(f"{gc.lat} {gc.lon}")
